c5a25348391ce0b441d4eb857611a62b3a5478a5
Cristian Lopez <cristianlopezr.931@gmail.com>
2022-05-21 15:38:05 -0400

    actualiza stock

--- src/app/productos/producto/producto.component.ts
+++ src/app/productos/producto/producto.component.ts
@@ -4,51 +4,77 @@ import { ActivatedRoute } from '@angular/router';
 import { switchMap, tap } from 'rxjs/operators';
 import { ProdSucursal } from '../productos-interfaces';
 
-
-
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
-  styleUrls: ['./producto.component.css']
+  styleUrls: ['./producto.component.css'],
 })
 export class ProductoComponent implements OnInit {
-
   prodSucursal: ProdSucursal = {
     producto: {
       _id: {
-        '$oid': ''
+        $oid: '',
       },
       nombre: '',
       precio: 0,
       marca: '',
       descripcion: '',
-      __v: 0
+      __v: 0,
     },
-    sucursales: [{
+    sucursales: [
+      {
         sucursal: '',
         nombre: '',
         stock: 0,
-    }]
-  }
+      },
+    ],
+  };
 
-  constructor(private prodService:ProductosService, 
-              private actRoute: ActivatedRoute) { }
+  constructor(
+    private prodService: ProductosService,
+    private actRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.getProducto();
   }
 
-  getProducto(){
+  getProducto() {
     this.actRoute.params
-      .pipe(
-        switchMap( (params) => this.prodService.getProducto(params['id']))
-      )
-      .subscribe(prodSucursal => {
-        this.prodSucursal = prodSucursal
+      .pipe(switchMap((params) => this.prodService.getProducto(params['id'])))
+      .subscribe((prodSucursal) => {
+        this.prodSucursal = prodSucursal;
         console.log(this.prodSucursal);
-        
-      }) 
+      });
   }
 
+  comprarProducto() {
+    let cantidad_compra = '';
+    let nombre_sucursal: string = (<HTMLInputElement>(
+      document.getElementById('sucursal')
+    )).value;
+    let id_sucursal: string = '';
 
+    cantidad_compra = (<HTMLInputElement>document.getElementById('cantidad'))
+      .value;
+
+    this.prodSucursal.sucursales.forEach((sucursal) => {
+      if (sucursal.nombre == nombre_sucursal) {
+        id_sucursal = sucursal.sucursal;
+      }
+    });
+
+    const producto = {
+      producto: this.prodSucursal.producto._id.$oid,
+      sucursal: id_sucursal,
+      cantidad: cantidad_compra,
+    };
+
+    this.prodService
+      .putProducto(this.prodSucursal.producto._id.$oid, producto)
+      .subscribe((data) => {
+        console.log(data);
+        this.getProducto();
+      });
+  }
 }
